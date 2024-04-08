364a12cf893aeea601719d452232f7d493d39f87
Youssef Kanjaa <ysf.knj@gmail.com>
2024-04-08 08:46:19 +0000

    nice

--- src/ExportDepartement.java
+++ src/ExportDepartement.java
@@ -5,12 +5,12 @@ import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class ExportEmploye extends BaseFrame {
+public class ExportDepartement extends BaseFrame {
     ButtonCard btnXls = new ButtonCard("Exporter XLS", "resources/xls.png", 180, 220, 200, 100);
     ButtonCard btnTxt = new ButtonCard("Exporter  Txt", "resources/txt.png", 420, 220, 200, 100);
 
-    public ExportEmploye() {
-        JLabel label = new JLabel("Export Emplyes table", JLabel.CENTER);
+    public ExportDepartement() {
+        JLabel label = new JLabel("Export Departement Table", JLabel.CENTER);
         label.setBounds(250, 100, 300, 25);
         add(label);
         setLayout(null);
@@ -29,18 +29,18 @@ public class ExportEmploye extends BaseFrame {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
         String formattedDateTime = now.format(formatter);
-        String defaultFilename = "export_emplyes_" + formattedDateTime + ".xls";
+        String defaultFilename = "export_departements_" + formattedDateTime + ".xls";
 
 
         JFileChooser fchoose = new JFileChooser();
         fchoose.setSelectedFile(new File(defaultFilename));
-        int option = fchoose.showSaveDialog(ExportEmploye.this);
+        int option = fchoose.showSaveDialog(ExportDepartement.this);
         if (option == JFileChooser.APPROVE_OPTION) {
             String name = fchoose.getSelectedFile().getName();
             String path = fchoose.getSelectedFile().getParentFile().getPath();
             String file = path + "\\" + name;
-            Employe.exportFileXls(file);
-            JOptionPane.showMessageDialog(ExportEmploye.this, "Exportation XLS réussie..");
+            Departement.exportFileXls(file);
+            JOptionPane.showMessageDialog(ExportDepartement.this, "Exportation XLS réussie..");
         }
 
     }
@@ -49,17 +49,16 @@ public class ExportEmploye extends BaseFrame {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
         String formattedDateTime = now.format(formatter);
-        String defaultFilename = "export_emplyes_" + formattedDateTime + ".txt";
+        String defaultFilename = "export_departements_" + formattedDateTime + ".txt";
         JFileChooser fchoose = new JFileChooser();
         fchoose.setSelectedFile(new File(defaultFilename));
-        int option = fchoose.showSaveDialog(ExportEmploye.this);
+        int option = fchoose.showSaveDialog(ExportDepartement.this);
         if (option == JFileChooser.APPROVE_OPTION) {
             String name = fchoose.getSelectedFile().getName();
             String path = fchoose.getSelectedFile().getParentFile().getPath();
             String file = path + "\\" + name;
-            Employe.exportFileTxt(file);
-
-            JOptionPane.showMessageDialog(ExportEmploye.this, "Exportation TXT réussie.");
+            Departement.exportFileTxt(file);
+            JOptionPane.showMessageDialog(ExportDepartement.this, "Exportation TXT réussie.");
         }
     }
 
@@ -73,3 +72,4 @@ public class ExportEmploye extends BaseFrame {
         }
     }
 }
+
